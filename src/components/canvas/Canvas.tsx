5eb1c25e4eede8433c1503751e4a9c354d0559af
vleis <valerij.leis@smail.th-koeln.de>
2020-12-06 22:06:35 +0100

    .sass added

---
+++ src/components/canvas/Canvas.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Canvas extends React.Component {
+    render() {
+       // return <h1>Hello, {this.props}</h1>;
+        return <canvas id="canvas-id" width="1000" height="1000"></canvas>;
+    }
+}
+
